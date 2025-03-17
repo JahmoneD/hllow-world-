12036fe44b5f5fb348650c382284ad4867b4fcb1
JahmoneD <jahmonedavis73@gmail.com>
2025-03-17 20:59:28 +0000

    lesen3

---
+++ madlib.py
@@ -0,0 +1,9 @@
+name=input("give me your name ")
+verb=input("give me a verb ")
+abject=input("give me a abject ")
+adjective=input("give me a adjective ")
+sound=input("give me a sound ")
+
+print(name+" whent to school"+"one day i whent to school and my teacher aks me what is verb and i didint and she punish me")
+
+print(name +" "+verb+" "+abject+ "whent to school "+ "one day i whent to school and my form teacher aks the class what is verb,abjectiv and adjective and no one anwser so she put hole class on ditention for the hole week. ")
