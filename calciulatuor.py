cb9d739b4be795eef43e5f6cdf7fefaaaa653453
JahmoneD <jahmonedavis73@gmail.com>
2025-04-01 20:16:07 +0000

    leson 1

--- calciulatuor.py
+++ calciulatuor.py
@@ -1,4 +1,8 @@
-numberone=input("whats the first number")
-symbol=input("what is the first symbol ")
-numbertwo=input("whats the first number")
-newnumberone=int(numberone)
+#name age sport
+name=input("what is your name ")
+age=input("what is your age ")
+sport=input("what is your favorit sport ") 
+print("your name is" +  name ) 
+print("your age is "  + age )
+print("your favorit sport is " + sport )
+
